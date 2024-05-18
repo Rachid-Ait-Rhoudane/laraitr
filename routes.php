62b15bbad4f0e216ed9395b59f74c71e0febfd1d
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2024-05-18 17:56:55 +0100

    add middleware to all routes

--- routes.php
+++ routes.php
@@ -12,24 +12,24 @@ $router->get('/contact', [HomeController::class, 'contact']);
 
 $router->get('/register', [RegistrationController::class, 'create'])->only('guest');
 
-$router->post('/register', [RegistrationController::class, 'store']);
+$router->post('/register', [RegistrationController::class, 'store'])->only('guest');
 
-$router->get('/login', [SessionController::class, 'create']);
+$router->get('/login', [SessionController::class, 'create'])->only('guest');
 
-$router->post('/login', [SessionController::class, 'store']);
+$router->post('/login', [SessionController::class, 'store'])->only('guest');
 
-$router->delete('/logout', [SessionController::class, 'destroy']);
+$router->delete('/logout', [SessionController::class, 'destroy'])->only('auth');
 
 $router->get('/notes', 'notes/index.php')->only('auth');
 
-$router->get('/note', 'notes/show.php');
+$router->get('/note', 'notes/show.php')->only('auth');
 
-$router->get('/notes/create', 'notes/create.php');
+$router->get('/notes/create', 'notes/create.php')->only('auth');
 
-$router->post('/notes/create', 'notes/store.php');
+$router->post('/notes/create', 'notes/store.php')->only('auth');
 
-$router->get('/notes/edit', 'notes/edit.php');
+$router->get('/notes/edit', 'notes/edit.php')->only('auth');
 
-$router->patch('/notes/edit', 'notes/update.php');
+$router->patch('/notes/edit', 'notes/update.php')->only('auth');
 
-$router->delete('/note', 'notes/destroy.php');
+$router->delete('/note', 'notes/destroy.php')->only('auth');
